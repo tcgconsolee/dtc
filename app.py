53dcf8f5f54762438595d08d205d7ec0cae98903
tcgconsolee <113512885+tcgconsolee@users.noreply.github.com>
2024-10-24 21:26:23 +0530

    everything else

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for, request
+from flask_login import LoginManager, UserMixin, login_user, current_user
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -6,11 +8,63 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SECRET_KEY"] = "ENTER YOUR SECRET KEY"
+db = SQLAlchemy()
+ 
+login_manager = LoginManager(app)
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True,
+                         nullable=False)
+    password = db.Column(db.String(250),
+                         nullable=False)
+ 
+ 
+db.init_app(app)
+ 
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+    
+        db.session.add(user)    
+        db.session.commit()
+    
+    
+        return redirect(url_for("login"))
+
+    return render_template("sign_up.html")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if 'loggedin' in session:
-        return redirect(url_for('index'))
-    return render_template('login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+    
+    
+        if user.password == request.form.get("password"):
+        
+            login_user(user)
+            return redirect(url_for("index"))
+    
+    
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
--- static/main.js
+++ static/main.js
@@ -1,9 +1,237 @@
-$("#to-proj").click(function(){
-    $(".home").slideUp(1000)
+// ANIMATIONS
+
+let active = ".home"
+
+$(".to-proj").click(function () {
+    if (active === ".projects") return;
+    $(active).slideUp(1000)
     $(".projects").show("slide", { direction: "down" }, 1000)
+    active = ".projects"
 })
-$(".evt-ctgory").each(function(i){
-    $(this).click(function() {
-        $(".evt-selected").css("left", (0.5 + (9.7*i)) + "em")
+$(".to-events").click(function () {
+    if (active === ".events") return;
+    $(active).slideUp(1000)
+    $(".events").show("slide", { direction: "down" }, 1000)
+    active = ".events"
+})
+$(".to-home").click(function () {
+    if (active === ".home") return;
+    $(active).slideUp(1000)
+    $(".home").show("slide", { direction: "down" }, 1000)
+    active = ".home"
+})
+$(".to-chat").click(function () {
+    if (active === ".chat") return;
+    $(active).slideUp(1000)
+    $(".chat").show("slide", { direction: "down" }, 1000)
+    active = ".chat"
+})
+
+// OTHER
+$(".project").each(function(i){
+    $(this).click(function () {
+        let project = $(this).clone()
+        let project_open =
+            `<div class = 'project-open'>
+                <div class = "cross">
+                    <img src = "../static/imgs/close.svg" alt = "">
+                </div>
+            </div>
+            <div class = "blurrer"></div>
+            `
+        $(".projects").append(project_open)
+        $(".project-open").prepend(project)
+        $(".cross").click(function(){
+            console.log("e")
+            $(".project-open").remove();
+            $(".blurrer").remove();
+        })
+        $(".project-open .project .pr-favs .hollow").click(function () {
+            $(".project-open .project .pr-favs .hollow").css("display", "none")
+            $(".project-open .project .pr-favs .filled").css("display", "block")
+            $(".hollow").eq(i).css("display", "none")
+            $(".filled").eq(i).css("display", "block")
+            addToWatchList($(".project-open .project .pr-name").html(), "project")
+        })
+    })
+})
+$(".dropdown").click(function() {
+    $(".pr-tags").each(function(i) {
+        if($(".dropdown").find(":selected").text().toLowerCase()=="all") {
+            $(".project").eq(i).css("display", "block");
+        } else {
+            if($(this).html().toLowerCase().includes($(".dropdown").find(":selected").text().toLowerCase())) {
+                $(".project").eq(i).css("display", "block");
+            } else {
+                $(".project").eq(i).css("display", "none");
+            }
+        }
+    })
+})
+$(".search-bar").click(function () {
+    if (active === ".events") return;
+    $(active).slideUp(1000)
+    $(".events").show("slide", { direction: "down" }, 1000)
+    active = ".events"
+    $(".events").scroll(1200)
+})
+$(".evt-ctgory").each(function (i) {
+    $(this).click(function () {
+        $("#events2").val("")
+        $(".evt-selected").css("display", "block")
+        $(".evt-selected").css("left", (0.5 + (9.7 * i)) + "em")
+        $(".event").each(function() {
+            console.log($(".evt-ctgory p").eq(i).html().toLowerCase())
+            console.log($(this).attr("data-type"))
+            if($(".evt-ctgory p").eq(i).html().toLowerCase()==$(this).attr("data-type")) {
+                $(this).css("display", "block")
+            } else {
+                $(this).css("display", "none")
+            }
+        })
+        
+    })
+})
+function sendMessage(msg) {
+    let code =
+        `
+    <div class="msg" id = "s">
+        <div class="msg-pic" id = "s-pic"></div>
+        <div class="msg-content" id = "s-msg">
+            <p>${msg}</p>
+        </div>
+    </div>
+    `
+    $(".msg-container").append(code)
+}
+$(".m-bar").keypress(function (e) {
+    if (e.which == 13) {
+        sendMessage($("#m").val())
+        $("#m").val("")
+    }
+})
+$("#groups").keypress(function (e) {
+    if (e.which == 13) {
+        $(".grp-name").each(function () {
+            if (!($(this).html().toLowerCase().includes($("#groups2").val().toLowerCase()))) {
+                $(this).parent().css("display", "none")
+            } else {
+                $(this).parent().css("display", "grid")
+            }
+        })
+    }
+})
+$("#projects1").keypress(function (e) {
+    if (e.which == 13) {
+        $(".pr-name").each(function () {
+            if (!($(this).html().toLowerCase().includes($("#projects2").val().toLowerCase()))) {
+                $(this).parent().css("display", "none")
+            } else {
+                $(this).parent().css("display", "grid")
+            }
+        })
+    }
+})
+$("#events1").keypress(function (e) {
+    if (e.which == 13) {
+        $(".evt-selected").css("display", "none")
+        $(".evt-name").each(function () {
+            if (!($(this).html().toLowerCase().includes($("#events2").val().toLowerCase()))) {
+                $(this).parent().css("display", "none")
+            } else {
+                $(this).parent().css("display", "block")
+            }
+        })
+    }
+})
+$(".notif-del").click(function () {
+    $(".notification").each(function () {
+        $(this).remove()
+    })
+})
+
+let active_notifs = false;
+$(".inactive").each(function (i) {
+    $(this).click(function () {
+        setTimeout(() => {
+            active_notifs = true;
+            $(this).css("display", "none")
+            $(".active").eq(i).css("display", "block")
+            $(".notifications").css("display", "block")
+        }, 100);
+    })
+})
+
+$(".active").each(function (i) {
+    $(this).click(function () {
+        active_notifs = false;
+        $(this).css("display", "none")
+        $(".inactive").eq(i).css("display", "block")
+        $(".notifications").css("display", "none")
+    })
+})
+
+$(document).click(function () {
+    if (active_notifs) {
+        active_notifs = false;
+        $(".active").each(function (i) {
+            $(this).css("display", "none")
+            $(".inactive").eq(i).css("display", "block")
+        })
+        $(".notifications").css("display", "none")
+    }
+})
+let active_heart = false;
+$(".nav-heart").each(function () {
+    $(this).click(function () {
+        if (active_heart) {
+            active_heart = false;
+            $(".watchlist").css("display", "none")
+        } else {
+            setTimeout(() => {
+                active_heart = true;
+                $(".watchlist").css("display", "block")
+            }, 100);
+        }
+    })
+})
+
+$(document).click(function () {
+    if (active_heart) {
+        active_heart = false;
+        $(".watchlist").css("display", "none")
+    }
+})
+function addToWatchList(name, type) {
+    let project_generator =
+        `
+<div class="watch-project">
+    <div class="watch-projpic"></div>
+    <div class="watch-projname">${name}</div>
+</div>
+`
+    let event_generator =
+        `
+<div class="watch-event">
+    <div class="watch-evepic"></div>
+    <div class="watch-evename">${name}</div>
+</div>
+`
+    if (type === "project") $(".watch-proj").append(project_generator);
+    if (type === "event") $(".watch-eve").append(event_generator);
+}
+$(".hollow").each(function (i) {
+    $(this).click(function () {
+        $(this).css("display", "none")
+        $(".filled").eq(i).css("display", "block")
+        addToWatchList($('.pr-name').eq(i).html(), "project")
+    })
+})
+$(".roundedh").click(function () {
+    addToWatchList($('.hevt-name').html(), "event")
+})
+$(".rounded").each(function (i) {
+    $(this).click(function () {
+        addToWatchList($('.evt-name').eq(i).html(), "event")
     })
 })
